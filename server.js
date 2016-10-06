4c95a1595f562d06e9432b9b7b658338974426bd
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-10-06 17:21:42 +0800

    added https

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 var express = require('express')
+var https = require('https')
+var fs = require('fs')
 var path = require('path')
+
 var app = express()
-const PORT = process.env.PORT || 4000
+
+var options = {
+  key: fs.readFileSync('certs/server.key'),
+  cert: fs.readFileSync('certs/server.crt')
+}
 
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url)
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    res.redirect('https://' + req.hostname + req.url)
   } else {
     next()
   }
 })
 
 app.use(express.static('public'))
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
 
-app.listen(PORT, function () {
-  console.log('Express server is up on port', PORT)
-})
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port', PORT)
+// })
+
+https.createServer(options, app).listen(8000)
