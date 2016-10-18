27b2220a398a8b00e5fb6159701716c56c2b51ae
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-10-18 22:02:17 +0800

    removed server

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express')
-var https = require('https')
-var fs = require('fs')
 var path = require('path')
+var app = express()
 const PORT = process.env.PORT || 4000
 
-var app = express()
-
-var options = {
-  key: fs.readFileSync('certs/server.key'),
-  cert: fs.readFileSync('certs/server.crt')
-}
-
 app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    res.redirect('https://' + req.hostname + req.url)
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url)
   } else {
     next()
   }
@@ -24,8 +16,6 @@ app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-// app.listen(PORT, function () {
-//   console.log('Express server is up on port', PORT)
-// })
-
-https.createServer(options, app).listen(PORT)
+app.listen(PORT, function () {
+  console.log('Express server is up on port', PORT)
+})
