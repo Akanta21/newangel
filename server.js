7a4adb435089248a4c6a87536830d412ca7832ac
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-11-06 22:25:01 +0800

    updated self-signed ssl for testing

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
+var https = require('https')
+var fs = require('fs')
 var express = require('express')
 var path = require('path')
 var app = express()
+
+var options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+}
 const PORT = process.env.PORT || 4000
 
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url)
-  } else {
-    next()
+function requireHTTPS(req, res, next) {
+  if(!req.secure) {
+    return res.redirect('https://' + req.get('host') + req.url)
   }
-})
+  next()
+}
+
+app.use(requireHTTPS);
 
 app.use(express.static('public'))
 app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-app.listen(PORT, function () {
-  console.log('Express server is up on port', PORT)
-})
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port', PORT)
+// })
+https.createServer(options, app).listen(5443)
