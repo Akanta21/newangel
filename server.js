ba11d712c3c3c10cf832f0ad0f06d7666f67241d
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-11-08 00:05:09 +0800

    https

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-var express = require('express')
-var http = require('http')
+var express = require('express'),
+   env = process.env.NODE_ENV || 'development';
 var path = require('path')
 var app = express()
 
 const PORT = process.env.PORT || 4000
 
-app.use(express.static('public'))
-
 function requireHTTPS(req, res, next) {
-  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production') {
-    return res.redirect('https://' + req.hotname + req.url)
+  if(req.headers['x-forwarded-proto'] != 'https') {
+    return res.redirect('https://' + req.get('host') + req.url)
   }
   next()
 }
 
-app.use(requireHTTPS)
+app.use(requireHTTPS);
 
+app.use(express.static('public'))
 app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-var server = http.createServer(app)
-
-server.listen(PORT, function () {
+app.listen(PORT, function () {
   console.log('Express server is up on port', PORT)
 })
 // https.createServer(options, app).listen(PORT)
