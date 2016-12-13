910b3362a414e4f92966be6393a80cfd9bf9b0a7
a.kanta22@gmail.com <a.kanta22@gmail.com>
2016-12-13 23:50:42 +0800

    new disc

--- public/components/cart/cart.controller.js
+++ public/components/cart/cart.controller.js
@@ -73,11 +73,13 @@ angular.module('angelApp')
     $scope.getDiscount = function() {
       console.log($scope.discountCode)
       console.log($scope.subtotal)
-      if($scope.discountCode == 'ai@fb' && $scope.subtotal > 800){
+      if($scope.discountCode == 'AICHRISTMAS' && $scope.subtotal > 800){
         $scope.discount = $scope.subtotal * 0.05
-        localStorage.setItem('discount', $scope.discount )
+        localStorage.setItem('discount', $scope.discount)
         return $scope.discount
       }
+      $scope.discount = 0
+      localStorage.setItem('discount', $scope.discount)
       return 0
     }
     $scope.getDelivery = function() {
